1bbe1cf24fb921323b495b3c24d054989afb8cce
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2021-03-26 12:06:26 -0700

    Delete files

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("#send-btn").on("click", function () {
-    console.log("send button clicked");
-    var message = $("#message").val().trim();
-    console.log(message);
-  });
-})
